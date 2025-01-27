1a2af9c0dd55e401877dc55542f15e7f96eef432
Benjamin <benne.terdin@hotmail.com>
2025-01-27 10:28:49 +0100

    s

---
+++ myfirstreact/pages/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TestPage = () => {
+    return (
+      <div>
+        <h1>Welcome to the Test Page!</h1>
+        <p>This is a simple test page to verify your Next.js setup.</p>
+      </div>
+    );
+  };
+  
+  export default TestPage;
